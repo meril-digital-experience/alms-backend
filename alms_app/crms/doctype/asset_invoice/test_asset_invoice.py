21c7e8b22921da10c956293b1c1812b2465c5665
Rishi-Hingad <rishi.hingad@merillife.com>
2026-08-06 10:27:02 +0530

    Change in module name

---
+++ alms_app/crms/doctype/asset_invoice/test_asset_invoice.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Rishi Hingad and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAssetInvoice(FrappeTestCase):
+	pass
